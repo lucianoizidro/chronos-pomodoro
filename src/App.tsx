e9fea4db2dc2eca153db74a9ae05c5e49f2b62fe
lucianoizidro <contato_luciano@hotmail.com>
2025-10-20 22:21:39 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+//PascalCase
+
+import {Fragment} from "react/jsx-runtime";
+
+/* Exportando a função */
+export function App(){
+    return (
+        <Fragment>
+            <h1>Desenvolvimento Web com React!</h1>
+            <p>Criando meu primeiro componente em React.</p>
+        </Fragment>
+    );
+}
+
+/* Export padrão*/
+// export default App;
+
+/* Export nomeado*/
+// export {App};
